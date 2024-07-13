965d17b4d7d333b311493fb61d325c643ee2079d
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-13 16:32:20 +0200

    [UPD] Create the array for posts

---
+++ ex-social-posts-starter/script.js
@@ -0,0 +1,10 @@
+// Creo un array con gli oggetti che dovrebbe contenere un post 
+const posts = [
+    {
+      author: "Phil Mangione",
+      time: "4 mesi fa",
+      text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+      image: "https://unsplash.it/600/300?image=171",
+      profilePic: "https://unsplash.it/300/300?image=15"
+    },
+];
